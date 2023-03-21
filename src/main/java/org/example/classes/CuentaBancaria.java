7cebf8b62cff6d4fcd384fbe7e481fdbdde5952d
CrisGadea <cristianhernangadea@gmail.com>
2023-03-21 20:06:47 -0300

    segundo commit: se agregó el .gitignore y los archivos del proyecto

---
+++ src/main/java/org/example/classes/CuentaBancaria.java
@@ -0,0 +1,27 @@
+package org.example.classes;
+
+public class CuentaBancaria {
+
+    private Usuario titular;
+
+    private double saldo;
+
+    public CuentaBancaria(Usuario usuario){
+        titular = usuario;
+    }
+
+    public Usuario getUsuario(){
+        return titular;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void depositar(double saldoADepositar){
+        if (saldoADepositar <= 0){
+            throw new IllegalArgumentException("La cantidad a depositar debe ser mayor que cero.");
+        }
+        saldo += saldoADepositar;
+    }
+}
